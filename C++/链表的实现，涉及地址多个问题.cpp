296c0c68b0a1d7b85477c157b74fa39374a1043e
Magneto <1527672216@qq.com>
2016-10-09 23:05:14 +0800

    finished
    终于搞定了，链表啊啊啊啊啊。许久不写代码很多问题都没有注意，不过也有许多新问题出现，内存不可读的情况还有断点问题，使用地址修改一般就不在需要return
    return
    return.....。。。。还有就是局部变量一旦在main函数中，似乎就会产生内存空间上的问题。传值调用一定要干干净净的。不能有太多牵连，这样会显得非常麻烦。当然许久又重写代码，也给写代码带来一定的困难。做复杂的问题之前一定要有个方案。不过还好之前写的代码问题分的比较清晰，逐个击破即可。之所以不喜欢用全局变量，是希望好好研究一下函数之间调用的问题。好啦，睡觉了

--- C++/链表的实现，涉及地址多个问题.cpp
+++ C++/链表的实现，涉及地址多个问题.cpp
@@ -1,5 +1,6 @@
 
 #include<iostream>//int
+#include<stdlib.h>
 using namespace std;
 typedef struct sets
 {
@@ -7,39 +8,43 @@ typedef struct sets
 	struct sets *next;
 	int howmany;
 
-}Sets,*XSets;
+}Sets, *XSets;
 void CreatSets(Sets *head);
-Sets And_Sets(Sets *Sets1, Sets *Sets2);
-Sets Set_Intersection(Sets *Sets1, Sets *Sets2);
-Sets Complementary_Sets(Sets *Sets1, Sets *Sets2);
+void And_Sets(Sets *Sets1, Sets *Sets2, XSets and_sets);
+void Set_Intersection(Sets *Sets1, Sets *Sets2, XSets Inter_sets);//似乎局部变量传值总是内存无法读取
+void Complementary_Sets(Sets *Sets1, Sets *Sets2, XSets Complementary);
 void PrintSets(Sets *Setss)
 {
-	Sets *p = Setss;
+	XSets p = Setss;
 	int n = p->howmany;
-	for (int i = 0; i <= n; i++)
+	for (int i = 0; i < n; i++)
 	{
 		p = p->next;
-		cout << p->elements<<' ';
+		cout << p->elements << ' ';
 
 	}
 };
 int main(int argc, char const *argv[])
 {
-	Sets Sets1;
-	//memset(&Sets1, 0, sizeof(Sets));
+	Sets *Sets1 = (Sets *)malloc(sizeof(Sets1));
+	//memset(&Sets1, 0, sizeof(Sets))
 	//(&Sets1)malloc(sizeof(Sets1));
-		CreatSets(&Sets1);
-        PrintSets(&Sets1);
-		Sets Sets2;
-		memset(&Sets2, 0, sizeof(Sets));
-		CreatSets(&Sets2);
-	
-	PrintSets(&Sets2);
-	Sets Set_And = And_Sets(&Sets1, &Sets2);
-//	PrintSets(&Set_And);
-	Sets Set_Inter = Set_Intersection(&Sets1, &Sets2);
-	Sets Set_Complem = Complementary_Sets(&Sets1, &Sets2);//不是结构体数组，这里传值需要特别注意
+	CreatSets(Sets1);
+	PrintSets(Sets1);
+	Sets *Sets2 = (Sets *)malloc(sizeof(Sets2));
+	//memset(&Sets2, 0, sizeof(Sets));
+	CreatSets(Sets2);
 
+	PrintSets(Sets2);
+	Sets *Set_And = (XSets)malloc(sizeof(Sets));
+	And_Sets(Sets1, Sets2, Set_And);
+	PrintSets(Set_And);
+	XSets Set_Inter = (XSets)malloc(sizeof(Sets));
+	Set_Intersection(Sets1, Sets2, Set_Inter);
+	PrintSets(Set_Inter);
+	XSets Set_Complem = (XSets)malloc(sizeof(Sets));
+	Complementary_Sets(Sets1, Sets2, Set_Complem);//不是结构体数组，这里传值需要特别注意
+	PrintSets(Set_Complem);
 	return 0;
 }
 void CreatSets(Sets *head)
@@ -53,32 +58,38 @@ void CreatSets(Sets *head)
 	head.elements = 0;
 	Sets *follow = head.next;//结构体需要初始化
 	*/
-    head = (Sets *)malloc(sizeof(*head));
-	Sets *follow = head->next;
-	head->howmany = n;
+	//head = (Sets *)malloc(sizeof(*head));
+	Sets *follow;
+	XSets p = head;
+	p->howmany = n;
 	for (int i = 0; i < n; i++)
 	{
 		follow = (Sets *)malloc(sizeof(*follow));//一定要开辟一个空间
 		cin >> follow->elements;
-		cout << follow->elements << "printf elements";
-		cout << endl;
+		//	cout << follow->elements << "printf elements";
+		//	cout << endl;
+		p->next = follow;
+		p = p->next;
 		follow = follow->next;
+
 	}
 	follow = NULL;
 	follow = head->next;
-	for(int i=0;i<n;i++)
+/*	for (int i = 0; i<n; i++)
 	{
-		cout<< follow->elements << "printf elements"<<endl;
-	}
+		cout << follow->elements << "printf elements" << endl;
+		follow = follow->next;
+	}*/
 	//return head;//局部变量问题
 }
-Sets And_Sets(Sets *Sets1, Sets *Sets2)
+void And_Sets(Sets *Sets1, Sets *Sets2, XSets and_sets)
 {
-	Sets and_sets;
+	//and_sets = (XSets)malloc(sizeof(Sets));
 	Sets *P1 = Sets1->next;
 	Sets *P2 = Sets2->next;
 	int number = 0;
-	Sets *P3 = &and_sets;
+	Sets *P3 = and_sets;
+	XSets instead;//初始化也有一定的限制
 	for (int i = 0; i<Sets1->howmany; i++)
 	{
 		int front_element = P1->elements;
@@ -93,8 +104,13 @@ Sets And_Sets(Sets *Sets1, Sets *Sets2)
 		if (j == Sets2->howmany)
 		{
 			number++;
+
+			//P3->next;
+			instead = (XSets)malloc(sizeof(Sets));
+			instead->elements = front_element;
+			instead->howmany = number;
+			P3->next = instead;
 			P3 = P3->next;
-			P3->elements = front_element;
 		}
 		P1 = P1->next;
 	}
@@ -102,45 +118,97 @@ Sets And_Sets(Sets *Sets1, Sets *Sets2)
 	for (int j = 0; j<Sets2->howmany; j++)
 	{
 		number++;
-		P3 = P3->next;
+
+		instead = (XSets)malloc(sizeof(Sets));
 		P2 = P2->next;
-		P3->elements = P2->elements;
+		instead->elements = P2->elements;
+		P3->next = instead;
+		P3 = P3->next;
+		instead->howmany = number;
+
 	}
-	P3->elements = number;
-	return *P3;
+	P3->next = NULL;
+	and_sets->howmany = number;
+	// for(int j = 0; j < number; j++)
+	{
+
+	}
+	//return and_sets;
 }
-Sets Set_Intersection(Sets *Sets1, Sets *Sets2)
+void Set_Intersection(Sets *Sets1, Sets *Sets2, XSets Inter_sets)
 {
-	Sets Inter_sets;
+	//	XSets Inter_sets = (XSets)malloc(sizeof(Sets));
 	Sets *P1 = Sets1;
 	Sets *P2 = Sets2;
 	int number = 0;
-	Sets *P3 = &Inter_sets;
-	for (int i = 0; i < Sets1->elements; i++)
+	Sets *P3 = Inter_sets;
+	XSets instead;
+	for (int i = 0; i < Sets1->howmany; i++)
 	{
 		P1 = P1->next;
 		int front_element = P1->elements;
-		for (int j = 0; j < Sets2->elements; j++)
+		for (int j = 0; j < Sets2->howmany; j++)
 		{
 			P2 = P2->next;
 			if (front_element == P2->elements)
 			{
-				P3 = P3->next;
+				instead = (XSets)malloc(sizeof(Sets));
+
 				number++;
-				P3->elements = front_element;
+				instead->elements = front_element;
+				P3->next = instead;
+				P3 = P3->next;
 				break;
 			}
 		}
 		P2 = Sets2;
 	}
-	Inter_sets.elements = number;
-	return Inter_sets;
+	Inter_sets->howmany = number;
+	//	return Inter_sets;
 }
-Sets Complementary_Sets(Sets *Sets1, Sets *Sets2)
+void Complementary_Sets(Sets *Sets1, Sets *Sets2, XSets Complementary)
 {
-	Sets Complementary;
-	memset(&Complementary, 0, sizeof(Complementary));
-	return Complementary;
+	//Complementary = (XSets)malloc(sizeof(Sets));
+	XSets instead;
+	XSets p1;
+	XSets last = Complementary;
+	XSets Set_Inter = (XSets)malloc(sizeof(Sets));
+	Set_Intersection(Sets1, Sets2, Set_Inter);
+	int k = 0,number,l=0;
+	int u = Sets1->howmany;
+	for (int i = 0; i < u; i++)
+	{
+		Sets1 = Sets1->next;
+		p1 = Set_Inter;
+		 l = 0;
+
+			 number = Sets1->elements;
+		for (int j = 0; j< Set_Inter->howmany; j++)
+		{
+
+			if (number == p1->elements)
+			{
+				
+				break;
+			}
+			p1 = p1->next;
+			l++;
+		}
+		if (l == Set_Inter->howmany)
+		{
+			instead = (XSets)malloc(sizeof(Sets));
+			instead->elements = number;
+			//	Complementary->next=instead;
+			//	Complementary=Complementary -> next;
+			last->next = instead;
+			last = last->next;
+			k++;
+		}
+	}
+	Complementary->howmany = k-1;
+	//memset(&Complementary, 0, sizeof(Complementary));
+
+	//	return Complementary;
 
 }
 
