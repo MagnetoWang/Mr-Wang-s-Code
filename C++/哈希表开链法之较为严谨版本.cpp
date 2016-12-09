bb7553f4e55201f81de039e44702e4a161d3eb84
Magneto <1527672216@qq.com>
2016-12-09 16:23:15 +0800

    完成版
    这次难度不大，稍微尝试了点新的东西，还有就是插入和创建可以为一个函数，在插入中设置一个条件就行了。用一个数组，内容是指针的数组大致就酱

--- C++/哈希表开链法之较为严谨版本.cpp
+++ C++/哈希表开链法之较为严谨版本.cpp
@@ -12,15 +12,60 @@ typedef struct data
 Data hash;
 }Hash_Map[];*/
 int key_number;//求余的哈希表
+/*
+13
+4
+28
+1 2 3 4 5 6 7 8 9 10 11 12 13  15 16 17 18 19 20 21 22 23 24 25 26  28 29 30
+1
+14
+2
+27
+1
+27
+3
+27
+1
+27
+0
+*/
+bool insert_map(Data *Hash_Map[], int number)
+{
+	Data *currents;
+	int positon = number%key_number;
+	currents = Hash_Map[positon]->next;
+	Data *ptr = Hash_Map[positon];
+	while (currents != NULL)
+	{
+		ptr = ptr->next;
+		currents = currents->next;
+	}
+	currents = (Data *)malloc(sizeof(Data));
+	currents->number = number;
+	currents->next = NULL;
+	ptr->next = currents;
+
+	return 1;
+}
 bool creat_hashmap(Data *Hash_Map[])
 {
 	int amount;
 	cin >> amount;
-	for (int i; i = 0; i++)
+	for (int i=0; i <amount; i++)
 	{
 		int number;
+		cin >> number;
 		insert_map(Hash_Map, number);
 	}
+	/*for (int i = 0; i < amount; i++)
+	{
+		Data *currents = Hash_Map[i];
+		while (currents!=NULL)
+		{
+			cout << currents->number << endl;
+			currents = currents->next;
+		}
+	}*/
 	return 0;
 }
 int search_in_HashMap(Data *Hash_Map[], int number)
@@ -28,32 +73,22 @@ int search_in_HashMap(Data *Hash_Map[], int number)
 	Data *currents;
 	int n = 0;
 	int positon = number%key_number;
-	currents = Hash_Map[positon]->next;
+	currents = Hash_Map[positon];
 	while (currents->number != number)
 	{
 		//ptr=currents;
 		n++;
 		currents = currents->next;
+		if (currents == NULL)
+			return 0;
 	}
+	
 	return n;
 }
-bool insert_map(Data *Hash_Map[],int number)
-{
-	Data *currents;
-	int positon = number%key_number;
-	currents = Hash_Map[positon]->next;
-	while (currents != NULL)
-	{
-		currents = currents->next;
-	}
-	currents = (Data *)malloc(sizeof(Data));
-	currents->number = number;
-	currents->next = NULL;
-	return 1;
-}
+
 bool delete_map(Data *Hash_Map[],int number)
 {
-	Data *currents, *ptr;
+	Data *currents, *ptr=NULL;
 	int positon = number%key_number;
 	currents = Hash_Map[positon]->next;
 	while (currents->number != number)
@@ -62,6 +97,8 @@ bool delete_map(Data *Hash_Map[],int number)
 		currents = currents->next;
 	}
 	//currents=(Data *)malloc(sizeof(Data));
+	if (ptr == NULL)
+		return 0;
 	ptr->next = currents->next;
 	free(currents);
 	//currents->number=number;
@@ -73,12 +110,18 @@ int main()
 {
 	//int amount,number;
 	int number;
+	cout << "此哈希表用于除余关键值，输入求余数" << endl;
 	cin >> key_number;
 	Data *Hash_Map[Maxsize];
-	for (int i = 0; i<Maxsize; i++)
+	Data *currents;
+	for (int i = 0; i<key_number+1; i++)
 	{
-		Hash_Map[i]->number = -1;
-		Hash_Map[i]->next = NULL;
+		/*(Hash_Map[i])->number = -1;
+		(Hash_Map[i])->next = NULL;*/
+		Hash_Map[i] = (Data *)malloc(sizeof(Data));
+		currents = Hash_Map[i];
+		currents->number = -1;
+		currents->next = NULL;//这里申请空间
 	}
 	/*for (int i = 0; i<amount; i++)
 	{
@@ -90,9 +133,10 @@ int main()
 	search_in_HashMap(Hash_Map,number);//这里返回查找次数
 	}*/
 	//上面的程序是需要比较死，我这次打算写一个比较灵活的
-	while (1)
+	int choice=10;
+	while (choice)
 	{
-		int choice;
+		
 		cout << "查找为1，插入为2，删除为3,创建为4，退出为0" << endl;
 			cin >> choice;
 		switch (choice)
@@ -136,4 +180,7 @@ int main()
 		}
 
 	}
+	cout << "programme is over" << endl;
+	system("pause");
+	return 0;
 }
