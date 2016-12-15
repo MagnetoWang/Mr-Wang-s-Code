8b92963154ee569f38e8a8137902513aae7ca280
Magneto <1527672216@qq.com>
2016-12-15 17:46:45 +0800

    如何理解树状数组以及线段树的基本代码
    三个主要的函数，lowbit  modify  get_sum.
    lowbit 获得数组的位置之后，在通过特殊的运算寻找路径。  modify
    初始化数组全部为0，开始输入数字和位置，然后只需要根据路径开始一个一个的相加即更新，不用考虑其他问题。。
    get_sum 同样知道位置后，按照特殊路径一个个相加，最后获得总值。。
    这样就很好的理解了，各个函数的作用和功能了
    这也可以作为看懂一个代码的好习惯和方法

--- C++/线段树以及树状数组的学习.cpp
+++ C++/线段树以及树状数组的学习.cpp
@@ -134,7 +134,8 @@ int lowbit(int x)
 {
 	return x&(-x);//求二进制的1的位置
 }
-void modify(int x,int add)
+void modify(int x,int add)//这里相当于建立树状数组，建立之前应该初始化数组，全部为0，然后
+                          //只需要在相应的位置相加即可，位置的路径只需要lowbit函数解决
 {
 
 	while(x<=MAXN)
@@ -152,4 +153,104 @@ int get_sum(int x)
 		x=lowbit(x);
 	}
 	return ret;
+}
+
+//线段树
+class regionTreeNode
+{
+public:
+	//regionTreeNode();
+	int left;
+	int right;
+	int cover;
+	regionTreeNode *leftChild;
+	regionTreeNode *rightChild;
+	regionTreeNodeg():left(0),right(0),cover(0),leftChild(NULL),rightChild(NULL){}
+	~regionTreeNode();
+	
+};
+
+regionTreeNode * creatReginTree(int min,int max)
+{
+	if(max-min<1)
+	{
+		cout<<"输入参数不合法"；
+		return NULL;
+	}
+	regionTreeNode *rootNode=new regionTreeNode();
+	rootNode->left=min;
+	rootNode->right=max;
+	rootNode->cover=0;
+	rootNode->leftChild=NULL;
+	rootNode->rightChild=NULL;
+	if(max-min==1)
+	{
+		return rootNode;
+	}
+	else
+		if(max-min>1)
+		{
+			int mid=(max+min)/2;
+			rootNode->leftChild=creatReginTree(min,mid);
+			rootNode->rightChild=creatReginTree(mid,max);
+		}
+	return rootNode
+}
+
+void insertRegin(int a,int b,regionTreeNode *tree)
+{
+	if(tree==NULL||a<tree->left||b>tree->right)
+	{
+		cout<<"输入参数不合法"；
+		return NULL;		
+	}
+	if(tree->left==a&&tree->right==b)
+	{
+		tree->cover++;
+		return;
+
+	}
+	int mid=(tree->left+tree->right)>>1;//这里运算不理解
+	if(b<=mid)
+	{
+		insertRegin(a,b,tree->leftChild);
+	}
+	else if(a>=mid)
+	{
+		insertRegin(a,b,tree->rightChild);
+	}
+	else
+	{
+		insertRegin(a,mid,tree->leftChild);
+		insertRegin(mid,b,tree->rightChild);
+	}
+}
+
+void deleteRegion(int a,int b,regionTreeNode *tree)
+{
+	if(tree==NULL||a<tree->left||b>tree->right)
+	{
+		cout<<"输入参数不合法"；
+		return NULL;		
+	}
+	if(tree->left==a&&tree->right==b)
+	{
+		tree->cover--;//怎样才叫被删除和添加啊
+		return;
+
+	}
+	int mid=(tree->left+tree->right)>>1;//这里运算不理解
+	if(b<=mid)
+	{
+		deletetRegin(a,b,tree->leftChild);
+	}
+	else if(a>=mid)
+	{
+		deleteRegin(a,b,tree->rightChild);
+	}
+	else
+	{
+		deleteRegin(a,mid,tree->leftChild);
+		deleteRegin(mid,b,tree->rightChild);
+	}
 }
